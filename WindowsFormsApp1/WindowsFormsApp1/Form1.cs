ed899496e898da203c795f2ecdcd36cc98917bd5
Neal <893399065@qq.com>
2018-05-04 22:10:20 +0800

    first

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mystr = "Data Source=.;Initial Catalog=studentDB;Integrated security=SSPI";
+            SqlConnection conn = new SqlConnection(mystr);
+            conn.Open();
+            if (conn.State == ConnectionState.Open)
+                MessageBox.Show("成功连接到数据库");
+            else
+                MessageBox.Show("不能连接到数据库");
+            conn.Close();
+        }
+    }
+}
